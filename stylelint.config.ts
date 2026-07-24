254f675faad306bb08f68b4b7adb20779916ff66
MorevM <max.seainside@gmail.com>
2026-07-24 17:54:57 +0300

    chore: Allow duplication of selectors within `reset`

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -4,4 +4,14 @@ export default defineConfig({
 	bem: {
 		files: ['./src/components/**/*.{css,scss}'],
 	},
+}, {
+	overrides: [
+		{
+			files: ['./src/reset/**/*.css'],
+			rules: {
+				// Keep semantic concerns separate even when selectors repeat.
+				'no-duplicate-selectors': null,
+			},
+		},
+	],
 });
